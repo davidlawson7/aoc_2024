fd56b8bc803f99e2969882799fd7b3cc5c527e31
David Lawson <david.lawson.95@outlook.com>
2024-12-05 13:44:44 +1000

    Got the bastard. Bytes man

--- day_04/day_04.go
+++ day_04/day_04.go
@@ -1,4 +1,4 @@
-package day_04
+package main
 
 import (
 	"bufio"
@@ -7,133 +7,13 @@ import (
 	"os"
 )
 
-type WSBoundsError struct {
-	row     int
-	col     int
-	message string
-}
-
-func (e *WSBoundsError) Error() string {
-	return fmt.Sprintf("%d, %d - %s", e.row, e.col, e.message)
-}
-
 type WordSearch struct {
-	Grid *[][]byte
+	Grid       *[][]string
+	Directions [][]int
 }
 
-func (w WordSearch) GetLetter(row int, col int) (letter byte, err error) {
-	// Do some out of bounds errorings... eyeroll
-	mRow, mCol := w.GetBounds()
-
-	if row > mRow || row < 0 || col > mCol || col < 0 {
-		return 'a', &WSBoundsError{row, col, "can't work with it"}
-	}
-
-	return (*w.Grid)[row][col], nil
-}
-
-func (w WordSearch) GetBounds() (row int, col int) {
-	return len((*w.Grid)) - 1, len((*w.Grid)[0]) - 1
-}
-
-func (w WordSearch) IsX(row int, col int) bool {
-	if letter, err := w.GetLetter(row, col); err == nil {
-		return letter == 'X'
-	}
-	return false // Should return a error as well if we get a out of bounds issue... lazy
-}
-
-func (w WordSearch) CheckHorizontal(row int, col int) bool {
-	if m, err := w.GetLetter(row, col+1); err == nil && m == 'M' {
-		if a, err := w.GetLetter(row, col+2); err == nil && a == 'A' {
-			if s, err := w.GetLetter(row, col+3); err == nil && s == 'S' {
-				return true
-			}
-		}
-	}
-	return false
-}
-
-func (w WordSearch) CheckBackwards(row int, col int) bool {
-	if m, err := w.GetLetter(row, col-1); err == nil && m == 'M' {
-		if a, err := w.GetLetter(row, col-2); err == nil && a == 'A' {
-			if s, err := w.GetLetter(row, col-3); err == nil && s == 'S' {
-				return true
-			}
-		}
-	}
-	return false
-}
-
-func (w WordSearch) CheckAbove(row int, col int) bool {
-	if m, err := w.GetLetter(row-1, col); err == nil && m == 'M' {
-		if a, err := w.GetLetter(row-2, col); err == nil && a == 'A' {
-			if s, err := w.GetLetter(row-3, col); err == nil && s == 'S' {
-				return true
-			}
-		}
-	}
-	return false
-}
-
-func (w WordSearch) CheckBelow(row int, col int) bool {
-	if m, err := w.GetLetter(row+1, col); err == nil && m == 'M' {
-		if a, err := w.GetLetter(row+2, col); err == nil && a == 'A' {
-			if s, err := w.GetLetter(row+3, col); err == nil && s == 'S' {
-				return true
-			}
-		}
-	}
-	return false
-}
-
-func (w WordSearch) CheckTopLeft(row int, col int) bool {
-	if m, err := w.GetLetter(row-1, col-1); err == nil && m == 'M' {
-		if a, err := w.GetLetter(row-2, col-2); err == nil && a == 'A' {
-			if s, err := w.GetLetter(row-3, col-3); err == nil && s == 'S' {
-				return true
-			}
-		}
-	}
-	return false
-}
-
-func (w WordSearch) CheckTopRight(row int, col int) bool {
-	if m, err := w.GetLetter(row-1, col+1); err == nil && m == 'M' {
-		if a, err := w.GetLetter(row-2, col+2); err == nil && a == 'A' {
-			if s, err := w.GetLetter(row-3, col+3); err == nil && s == 'S' {
-				return true
-			}
-		}
-	}
-	return false
-}
-
-func (w WordSearch) CheckBottomLeft(row int, col int) bool {
-	if m, err := w.GetLetter(row+1, col-1); err == nil && m == 'M' {
-		if a, err := w.GetLetter(row+2, col-2); err == nil && a == 'A' {
-			if s, err := w.GetLetter(row+3, col-3); err == nil && s == 'S' {
-				return true
-			}
-		}
-	}
-	return false
-}
-
-func (w WordSearch) CheckBottomRight(row int, col int) bool {
-	if m, err := w.GetLetter(row+1, col+1); err == nil && m == 'M' {
-		if a, err := w.GetLetter(row+2, col+2); err == nil && a == 'A' {
-			if s, err := w.GetLetter(row+3, col+3); err == nil && s == 'S' {
-				return true
-			}
-		}
-	}
-	return false
-}
-
-// Transforms raw input from a text file into something workable for this test.
-func GetInput(filePath *string) *[][]byte {
-	file, err := os.Open(*filePath)
+func WordSearchInit(filePath string) *WordSearch {
+	file, err := os.Open(filePath)
 
 	if err != nil {
 		log.Fatal(err)
@@ -141,74 +21,74 @@ func GetInput(filePath *string) *[][]byte {
 
 	fileScanner := bufio.NewScanner(file)
 	fileScanner.Split(bufio.ScanLines)
-	var wordSearch [][]byte
+	var wordSearch [][]string
 
 	for fileScanner.Scan() {
-		wordSearch = append(wordSearch, fileScanner.Bytes())
+		var row []string
+		for _, byte := range fileScanner.Bytes() {
+			row = append(row, string(byte))
+		}
+		wordSearch = append(wordSearch, row)
 	}
 
 	file.Close()
 
-	return &wordSearch
+	return &WordSearch{
+		Grid: &wordSearch,
+		Directions: [][]int{
+			{0, 1},   // Left to right
+			{0, -1},  // Right to left
+			{-1, 0},  // Up
+			{1, 0},   // Down
+			{-1, 1},  // Diagonal top-left
+			{-1, -1}, // Diagonal top-right
+			{1, 1},   // Diagonal bottom-left
+			{1, -1},  // Diagonal bottom-right
+		},
+	}
 }
 
-// Checks for the word XMAS starting from (aka letter X) the given position
-// (x & y) within the wordSearch.
-func Check(w *WordSearch, row int, col int) int {
-	if !w.IsX(row, col) {
-		return 0
-	}
-
-	xmasCount := 0
-
-	if w.CheckBackwards(row, col) {
-		xmasCount += 1
-	}
-
-	if w.CheckHorizontal(row, col) {
-		xmasCount += 1
-	}
-
-	if w.CheckAbove(row, col) {
-		xmasCount += 1
-	}
-
-	if w.CheckBelow(row, col) {
-		xmasCount += 1
-	}
-
-	if w.CheckTopLeft(row, col) {
-		xmasCount += 1
-	}
-
-	if w.CheckTopRight(row, col) {
-		xmasCount += 1
-	}
-
-	if w.CheckBottomLeft(row, col) {
-		xmasCount += 1
-	}
-
-	if w.CheckBottomRight(row, col) {
-		xmasCount += 1
-	}
-
-	return xmasCount
+func (w WordSearch) GetBounds() (row int, col int) {
+	return len((*w.Grid)) - 1, len((*w.Grid)[0]) - 1
 }
 
-func CountAllWords(filePath *string) int {
-	wordSearch := GetInput(filePath)
-	w := WordSearch{
-		Grid: wordSearch,
+func (w WordSearch) GetLetter(row int, col int) string {
+	mRow, mCol := w.GetBounds()
+	if (row > mRow || row < 0) || (col > mCol || col < 0) {
+		return "" // Just return a empty string, will make any check fail. Could error handle if less lazy
 	}
+	return (*w.Grid)[row][col]
+}
 
-	r, c := w.GetBounds()
-	sum := 0
+func (w WordSearch) CheckDirection(row int, col int, dr int, dc int) bool {
+	return (w.GetLetter(row, col) + w.GetLetter(row+(1*dr), col+(1*dc)) + w.GetLetter(row+(2*dr), col+(2*dc)) + w.GetLetter(row+(3*dr), col+(3*dc))) == "XMAS"
+}
 
-	for i := 0; i < r+1; i++ {
-		for j := 0; j < c+1; j++ {
-			sum = sum + Check(&w, i, j)
+func (w WordSearch) CheckEachDirection(row int, col int) int {
+	count := 0
+	if l := w.GetLetter(row, col); l == "X" {
+		for _, d := range w.Directions {
+			if w.CheckDirection(row, col, d[0], d[1]) {
+				count++
+			}
 		}
 	}
-	return sum
+	return count
+}
+
+func (w WordSearch) FindAllXMAS() int {
+	r, c := w.GetBounds()
+	wc := 0
+	for i := 0; i <= r; i++ {
+		for j := 0; j <= c; j++ {
+			wc = wc + w.CheckEachDirection(i, j)
+		}
+	}
+	return wc
+}
+
+func main() {
+	w := WordSearchInit("day_04/input.txt")
+	wc := w.FindAllXMAS()
+	fmt.Printf("Word Count: %d\n", wc)
 }
